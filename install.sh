cc9b4cb0f900174b698c0e48127d07508843d4be
J J Sloan <jjs@mainphrame.com>
2020-03-30 15:04:35 -0700

    added top level install helper script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# get os
+if [ -f /etc/redhat-release ]; then
+  export LXTYPE=RHEL
+elif [ -f /etc/SuSE-release ]; then
+  export LXTYPE=SUSE
+elif [ -f /etc/SUSE-brand ]; then
+  export LXTYPE=SUSE
+elif [ -f /etc/debian_version ]; then
+  export LXTYPE=DEBIAN
+elif [ -f /etc/gentoo-release ]; then
+  export LXTYPE=GENTOO
+fi
+
+if [ $LXTYPE == "RHEL" ]; then
+  RREL=`grep -i version_id /etc/os-release | awk -F\= '{ print $2 }'`
+
+  REL7=`echo $RREL | grep 7|wc -w`
+  REL8=`echo $RREL | grep 8|wc -w`
+  if [ $REL7 -eq 1 ]; then
+    LVL=7
+  elif [ $REL8 -eq 1 ]; then
+    LVL=8
+    echo "centos/rhel version $LVL detected"
+    echo "copy & paste the following command to begin the install:"
+    echo
+    echo " bash install-centos$LVL.sh | tee installer.log 2>&1"
+    echo
+  fi
+  else
+    echo "$LXTYPE type system detected, installer not present"
+    echo "refer to manual-install.txt for instructions"
+fi
+
+
