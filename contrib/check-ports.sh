81a2190870777b525507c9f4d648ce97107c3d39
J J Sloan <jjs@mainphrame.com>
2020-04-05 14:06:40 -0700

    template and contribe cleanups

--- contrib/check-ports.sh
+++ contrib/check-ports.sh
@@ -7,3 +7,22 @@ $ netstat -tulp4n | grep LISTEN| grep -e 25 -e 3310 -e 10024
 # or this 
 $ lsof -i -P -n  | grep IPv4 |grep LISTEN| grep -e 25 -e 3310 -e 10024
 
+#
+# if secure smtp is required -
+#
+
+# submission port
+$ openssl s_client -connect remote.example.com:587 -starttls smtp
+
+# smtps port -
+$ openssl s_client -connect remote.example.com:465
+
+#
+# the nagios way 
+# make sure that both tests below are defined in commands.cfg
+#
+./check_smtp -H remote.example.com -p 587 -S
+SMTP OK - 0.329 sec. response time|time=0.328936s;;;0.000000
+
+./check_ssmtp -H remote.example.com -p 465 -S
+SSMTP OK - 0.171 second response time on port 465 [220 remote.example.com Postfix ready]|time=0.171020s;;;0.000000;10.000000
