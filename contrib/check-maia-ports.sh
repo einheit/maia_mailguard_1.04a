a0cd1fc7f6ecd1805703393f608bfa9fc31616ac
J J Sloan <jjs@mainphrame.com>
2020-05-23 10:12:06 -0700

    Added first cut of a maia port check script

---
+++ contrib/check-maia-ports.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# check for maia ports 
+
+tmp=$(which ss &>/dev/null)
+if [ $? == 0 ]; then
+  ss -tulw4n|grep -e 25 -e 3310 -e 10024
+else
+  tmp=$(which lsof &>/dev/null)
+  if [ $? == 0 ]; then
+    lsof -i -P -n  | grep IPv4 |grep LISTEN| grep -e 25 -e 3310 -e 10024
+  else
+    tmp=$(which netstat &>/dev/null)
+    if [ $? == 0 ]; then
+      netstat -tulpn | grep LISTEN| grep -e 25 -e 3310 -e 10024
+    fi
+  fi 
+fi
+
