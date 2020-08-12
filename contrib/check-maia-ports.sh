3bb6dc6de3b2c0b9f2d8674661c7bc3371952a57
J J Sloan <jjs@mainphrame.com>
2020-08-12 10:59:37 -0700

    Added port 465 to maia ports check script

--- contrib/check-maia-ports.sh
+++ contrib/check-maia-ports.sh
@@ -3,15 +3,15 @@
 
 tmp=$(which lsof &>/dev/null)
 if [ $? == 0 ]; then
-  lsof -i -P -n  | grep IPv4 |grep LISTEN| grep -e 25 -e 3310 -e 10024
+  lsof -i -P -n  | grep IPv4 |grep LISTEN| grep -e 25 -e 465 -e 3310 -e 10024
 else
   tmp=$(which netstat &>/dev/null)
   if [ $? == 0 ]; then
-    netstat -tulpn | grep LISTEN| grep -e 25 -e 3310 -e 10024
+    netstat -tulpn | grep LISTEN| grep -e 25 -e 465 -e 3310 -e 10024
   else
     tmp=$(which ss &>/dev/null)
     if [ $? == 0 ]; then
-      ss -tulw4n|grep -e 25 -e 3310 -e 10024
+      ss -tulw4n|grep -e 25 -e 465 -e 3310 -e 10024
     fi 
   fi
 fi
